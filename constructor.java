d399a19588475c067db0dfe78f46199da6b678e1
AKASH B <147173640+Akash242005@users.noreply.github.com>
2024-12-30 11:00:31 +0530

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -1,4 +1,5 @@
-public class parameterConstructor{
+import java.util.*;
+class parameterConstructor{
 	int roll;
 	String name;
 	// PARAMETER CONSTRUCTOR
@@ -21,5 +22,17 @@ public class parameterConstructor{
 	}
 	public void display(){
 		System.out.println("The name is "+this.name+" and the roll number is "+this.roll);
+	}
+}
+public class Main{
+	public static void main(String[] args){
+		parameterConstructor first = new parameterConstructor(7,"Akash");
+		first.display();
+		parameterConstructor second = new parameterConstructor(52,"Ganapathi");
+		second.display();
+		parameterConstructor firstcopy = new parameterConstructor(first);
+		firstcopy.display();
+		parameterConstructor third = new parameterConstructor();
+		third.display();
 	}
 }
